7a323ae75713084f617323e289e8a01163be3319
Kairat Sultan <garri.sultonov@mail.ru>
2023-03-08 04:00:54 +0600

    change api routes

--- utils.py
+++ utils.py
@@ -27,25 +27,27 @@ async def get_or_create_user(tg_id: str, username: str, first_name: str, last_na
             "first_name": first_name,
             "last_name": last_name,
         }
-        async with session.post(API_URL, json=data) as resp:
+        async with session.post(API_URL + '/api/tgUser/', json=data) as resp:
             response = await resp.json()
-            user = make_dataclass("TgUser", ['tg_id', 'username', 'first_name', 'last_name'])
-            user_obj = user(response['tg_id'],
-                            response['username'],
-                            response['first_name'],
-                            response['last_name'])
-            return user_obj
+            if response.status == 201:
+                user = make_dataclass("TgUser", ['tg_id', 'username', 'first_name', 'last_name'])
+                user_obj = user(response['tg_id'],
+                                response['username'],
+                                response['first_name'],
+                                response['last_name'])
+                return user_obj
 
 
 async def upload_updates_to_server(tg_id: str, json: dict) -> UserForcing:
     async with aiohttp.ClientSession() as session:
         data = {
-            "tg_user": tg_id,
+            "tg_user_id": tg_id,
             "json": json
         }
-        async with session.post(API_URL, json=data) as resp:
+        async with session.post(API_URL + '/api/statUser/', json=data) as resp:
             response = await resp.json()
-            forcing = make_dataclass("UserForcing", ['tg_id'])
-            forcing_obj = forcing(response['tg_id'])
-            return forcing_obj
+            if response.status_code == 201:
+                forcing = make_dataclass("UserForcing", ['id'])
+                forcing_obj = forcing(response['id'])
+                return forcing_obj
 
